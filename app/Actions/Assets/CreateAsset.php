2f6b46ae4864756e3c78d7211d9802c70845065e
spencerrlongg <spencer@spencerlong.com>
2023-10-05 13:11:17 -0500

    boilerplate, package installed

---
+++ app/Actions/Assets/CreateAsset.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Actions\Assets;
+
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class CreateAsset
+{
+    use AsAction;
+
+    public function handle()
+    {
+        // ...
+    }
+}
