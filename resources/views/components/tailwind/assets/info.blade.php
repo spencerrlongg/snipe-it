0849432740f930405e1527ebc7e1bd18201453ae
spencerrlongg <spencer@spencerlong.com>
2024-06-03 00:41:19 -0500

    icons in components, attempt at dl component

--- resources/views/components/tailwind/assets/info.blade.php
+++ resources/views/components/tailwind/assets/info.blade.php
@@ -2,16 +2,25 @@
 <div>
     <h1 class="text-2xl m-4">View Asset {{ $asset->asset_tag }}</h1>
     <div class="ml-4 grid grid-cols-1 md:grid-cols-2 md:gap-2">
+        {{--        hmm, not sure the best way to componentize this... //shrug will ask marcus--}}
+        {{--        <x-tailwind.datalist>--}}
+        {{--            <x-slot name="label">--}}
+        {{--                Status--}}
+        {{--            </x-slot>--}}
+        {{--            <x-slot name="data">--}}
+        {{--                @if(@$asset->assetstatus->deployable)--}}
+        {{--                   <x-tailwind.check-icon-filled/>--}}
+        {{--                @else--}}
+        {{--                    yellow--}}
+        {{--                @endif--}}
+        {{--                {{ $asset->assetstatus->name }}--}}
+        {{--            </x-slot>--}}
+        {{--        </x-tailwind.datalist>--}}
         <dl class="grid grid-cols-2 border rounded-md">
             <dt class="font-bold bg-gray-200 pl-2">Status</dt>
             <dd class="bg-gray-200 flex items-center">
-                @if(@$asset->assetstatus->deployable)
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                         class="text-green size-5">
-                        <path fill-rule="evenodd"
-                              d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
-                              clip-rule="evenodd"/>
-                    </svg>
+                @if($asset->assetstatus->deployable)
+                    <x-tailwind.check-icon-filled/>
                 @else
                     yellow
                 @endif
@@ -28,20 +37,10 @@
             <dt class="font-bold pl-2">BYOD</dt>
             <dd class="flex items-center">
                 @if($asset->byod)
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                         class="size-5 text-green">
-                        <path fill-rule="evenodd"
-                              d="M19.916 4.626a.75.75 0 0 1 .208 1.04l-9 13.5a.75.75 0 0 1-1.154.114l-6-6a.75.75 0 0 1 1.06-1.06l5.353 5.353 8.493-12.74a.75.75 0 0 1 1.04-.207Z"
-                              clip-rule="evenodd"/>
-                    </svg>
+                    <x-tailwind.check-icon/>
                     Yes
                 @else
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                         class="size-5 text-deletered">
-                        <path fill-rule="evenodd"
-                              d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z"
-                              clip-rule="evenodd"/>
-                    </svg>
+                    <x-tailwind.x-icon/>
                     No
                 @endif
             </dd>
---
+++ resources/views/components/tailwind/datalist.blade.php
@@ -0,0 +1,4 @@
+<dl class="grid grid-cols-2 border rounded-md">
+    <dt class="font-bold bg-gray-200 flex items-center">{{ $label }}</dt>
+    <dd class="bg-gray-200">{{ $data }}</dd>
+</dl>
