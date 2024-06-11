1f26b74bd94a08440b873eb7800aff557a0bec4d
Marcus Moore <contact@marcusmoore.io>
2024-06-11 13:21:33 -0700

    Remove old datalist

--- resources/views/components/tailwind/assets/info.blade.php
+++ resources/views/components/tailwind/assets/info.blade.php
@@ -2,50 +2,6 @@
 <div>
 
 <div class="ml-4 grid grid-cols-1 md:grid-cols-2 md:gap-2">
-        {{--        hmm, not sure the best way to componentize this... //shrug will ask marcus--}}
-        {{--        <x-tailwind.datalist>--}}
-        {{--            <x-slot name="label">--}}
-        {{--                Status--}}
-        {{--            </x-slot>--}}
-        {{--            <x-slot name="data">--}}
-        {{--                @if(@$asset->assetstatus->deployable)--}}
-        {{--                   <x-tailwind.check-icon-filled/>--}}
-        {{--                @else--}}
-        {{--                    yellow--}}
-        {{--                @endif--}}
-        {{--                {{ $asset->assetstatus->name }}--}}
-        {{--            </x-slot>--}}
-        {{--        </x-tailwind.datalist>--}}
-        <dl class="grid grid-cols-2 border rounded-md">
-            <dt class="font-bold bg-gray-200 pl-2">Status</dt>
-            <dd class="bg-gray-200 flex items-center">
-                @if($asset->assetstatus->deployable)
-                    <x-tailwind.icons.check-icon-filled/>
-                @else
-                    {{--                    <x-tailwind.icons.check-icon-filled class="text-yellow"--}}
-                    yellow
-                @endif
-                {{ $asset->assetstatus->name }}
-            </dd>
-            <dt class="font-bold pl-2">Serial</dt>
-            <dd class="">{{ $asset->serial }}</dd>
-            <dt class="font-bold bg-gray-200 pl-2">Category</dt>
-            <dd class="bg-gray-200">{{ $asset->model->category->name }}</dd>
-            <dt class="font-bold pl-2">Model</dt>
-            <dd class="">{{ $asset->model->name }}</dd>
-            <dt class="font-bold bg-gray-200 pl-2">Model No.</dt>
-            <dd class="bg-gray-200">{{ $asset->model->model_number }}</dd>
-            <dt class="font-bold pl-2">BYOD</dt>
-            <dd class="flex items-center">
-                @if($asset->byod)
-                    <x-tailwind.icons.check-icon/>
-                    Yes
-                @else
-                    <x-tailwind.icons.x-icon/>
-                    No
-                @endif
-            </dd>
-        </dl>
 
         <x-tailwind.dl>
             <x-tailwind.dl.item flex>
