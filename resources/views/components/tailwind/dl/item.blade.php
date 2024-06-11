8e8bf0eb7e7b2ab8bf2c130f9165d92c2a32c1dc
Marcus Moore <contact@marcusmoore.io>
2024-06-11 13:19:17 -0700

    Inline padding

--- resources/views/components/tailwind/dl/item.blade.php
+++ resources/views/components/tailwind/dl/item.blade.php
@@ -5,11 +5,11 @@
     'flex' => false,
 ])
 
-<div class="grid grid-cols-2 odd:bg-gray-200">
+<div class="grid grid-cols-2 odd:bg-gray-200 px-2">
     @if ($title instanceof ComponentSlot)
-        <dt {{ $title->attributes->class(['font-bold pl-2']) }}>{{ $title }}</dt>
+        <dt {{ $title->attributes->class(['font-bold']) }}>{{ $title }}</dt>
     @else
-        <dt class="font-bold pl-2">{{ $title }}</dt>
+        <dt class="font-bold">{{ $title }}</dt>
     @endif
     <dd @class(['flex items-center' => $flex])>
         {{ $slot }}
