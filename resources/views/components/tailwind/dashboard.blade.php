331aa2ee6e2561f0c76051eeed33f7e6786b3dac
spencerrlongg <spencer@spencerlong.com>
2024-06-13 12:51:10 -0500

    make marcus happy with blade livewire components

--- resources/views/components/tailwind/dashboard.blade.php
+++ resources/views/components/tailwind/dashboard.blade.php
@@ -1,8 +1,11 @@
+@php
+    use App\Models\Asset;
+@endphp
 <x-tailwind.layout>
     <h1 class="text-2xl">Dashboard</h1>
-    <div class="grid grid-cols-2 gap-4">
-        @livewire(\App\Livewire\ThisYearComponentsLicensesChart::class)
-        @livewire(\App\Livewire\ThisYearCheckoutsChart::class)
+    <div class="grid grid-cols-3 gap-4">
+        <livewire:this-year-components-licenses-chart/>
+        <livewire:this-year-checkouts-chart/>
+        <livewire:filament-history-table :asset="Asset::find(1)"/>
     </div>
-
 </x-tailwind.layout>
